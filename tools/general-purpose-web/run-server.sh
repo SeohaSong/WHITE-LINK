092de4929a69c179e24c130c006f9e3be24f68c0
seohasong <tisutoo@gmail.com>
2020-02-10 02:44:35 +0900

    continue previous working

--- tools/general-purpose-web/run-server.sh
+++ tools/general-purpose-web/run-server.sh
@@ -6,14 +6,6 @@
     #     openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -days 365
     #     openssl x509 -outform der -in cert.pem -out cert.crt
     # ) fi
-    lines=( $( ps -ax | grep http.server ) )
-    kill $lines &> /dev/null
-    (
-        cd dist
-        python3 -m http.server
-    ) &
-    path=${HOST_SHS_PATH%/*}
-    bat_filepath="$path/Anaconda3/Scripts/activate.bat"
-    anaconda_path="$path/Anaconda3"
-    { cmd.exe /c "$bat_filepath $anaconda_path & python tools/main.py"; } &
+    cd dist
+    shs run python -m http.server -b 0.0.0.0
 )
