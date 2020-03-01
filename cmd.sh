92c00f69789a6c4cfe31c7878d297c2b5f0757d0
seohasong <tisutoo@gmail.com>
2020-03-02 06:51:47 +0900

    continue previous working

--- cmd.sh
+++ cmd.sh
@@ -2,17 +2,18 @@ main() {
     local args=( $@ )
     local arg=${args[0]}
     local adpath=$( dirname "${BASH_SOURCE:-${(%):-%x}}" )
-    local core=. cmd=$SHELL
+    local core=. cmd='bash -c'
     if [ -d "$adpath/core" ]; then core=core; fi
-    if [[ "$arg" =~ ^__.*__$ ]]; then cmd=.; fi
+    if [[ "$arg" =~ ^__.*__$ ]]; then cmd=eval; fi
     local nxt_args="$core ${args[@]}"
-    . "$adpath/$core/tools/cmd/init.sh" "$nxt_args"
+    . $adpath/$core/tools/cmd/init.sh "$nxt_args"
     if [ -f "$__APATH__" ]
-    then $cmd "$__APATH__" "${args[@]:1}"
-    elif [ -f "$adpath/core/cmd/$arg/main.sh" ]
-    then . "$adpath/core/cmd.sh" ${args[@]}
-    else . "$adpath/$core/tools/cmd/help.sh" "$adpath"
+    then $cmd 'main() { . $__APATH__; }; main'
+    elif [ -f $adpath/core/cmd/$arg/main.sh ]
+    then . $adpath/core/cmd.sh ${args[@]}
+    else . $adpath/$core/tools/cmd/help.sh
     fi
+    . $adpath/$core/tools/cmd/close.sh
 }
 main $@
 unset -f main
