5d3016bc9deb968529d896259b1abf27c2f6f5ae
seohasong <tisutoo@gmail.com>
2020-03-06 02:56:03 +0900

    continue previous working

--- cmd/upload/main.sh
+++ cmd/upload/main.sh
@@ -1,14 +1,7 @@
-main() {
-    shs __trap__
-    local adpath v
-    local dpaths="./core ."
-    for adpath in $( for v in $dpaths; do ( cd $v && pwd ); done )
-    do (
-        shs __echo__ Update \'$adpath\' repo
-        cd $adpath
-        git add .
-        git commit -m "continue previous working" || :
-        git push origin master
-    ) done
-}
-main
+local CMD_PATH=$__CMD_PATH__
+wl __trap__
+wl echo Update \'$CMD_PATH\' repo
+cd $CMD_PATH
+git add .
+git commit -m "continue previous working" || :
+git push origin master
