0ec45d31cc7e2036377f34225140c3ed70567020
seohasong <tisutoo@gmail.com>
2020-03-08 02:23:53 +0900

    continue previous working

--- cmd/upload/main.sh
+++ cmd/upload/main.sh
@@ -1,7 +1,5 @@
-local CMD_PATH=$__CMD_PATH__
-wl __trap__
-wl echo Update \'$CMD_PATH\' repo
-cd $CMD_PATH
+wl echo Update \'$DDD_PATH\' repo
+cd $DDD_PATH
 git add .
 git commit -m "continue previous working"
 git push origin master
